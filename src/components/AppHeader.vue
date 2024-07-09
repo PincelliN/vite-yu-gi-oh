9dc8500dea764e5b4c94bbdd78f911ba22acffab
Nicolò Pincelli <pincelli.n@gmail.com>
2024-07-09 15:46:24 +0200

    add file start

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "AppHeader"
+}
+</script>
+
+<template>
+  <div class="row">
+    <div class="col-1">
+      <img class="img-fluid" src="" alt="">
+    </div>
+    <div class="col-11">
+      <h1>Yo-gi-oh</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
