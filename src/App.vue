af6e130a81a9407b577a4cff1c942090ac0e03cf
Nicolò Pincelli <pincelli.n@gmail.com>
2024-07-09 16:12:44 +0200

    Add AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 
+export default {
+  name: "App",
+  components: {
+    AppHeader
+  }
+}
 </script>
 
 <template class="container">
-
+  <AppHeader />
 </template>
 
 <style lang="scss">
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,18 @@
 <script>
 export default {
-  name: "AppHeader"
+  name: "AppHeader",
 }
 </script>
 
 <template>
   <div class="row">
-    <div class="col-1">
-      <img class="img-fluid" src="" alt="">
+    <div class="col-1 d-flex align-items-center">
+      <img class="img-fluid"
+        src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ecd51263-bb0f-4465-af86-f7ed25e58e70/degn1d8-963da678-da92-4dbd-acc5-8c9e9a9b34e2.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2VjZDUxMjYzLWJiMGYtNDQ2NS1hZjg2LWY3ZWQyNWU1OGU3MFwvZGVnbjFkOC05NjNkYTY3OC1kYTkyLTRkYmQtYWNjNS04YzllOWE5YjM0ZTIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.HSiLqBk_Qlby8vbuIwv0M7MLx3jkoGsv8N45zDQ_Tg8"
+        alt="">
     </div>
     <div class="col-11">
-      <h1>Yo-gi-oh</h1>
+      <h1>Yu-gi-oh</h1>
     </div>
   </div>
 </template>
